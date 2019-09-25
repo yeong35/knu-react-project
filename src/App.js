4fefe7c33422d87f5e4f27307013190693dd0901
171225h <e_yeong@naver.com>
2019-09-25 18:25:39 +0900

    initial commit

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload. 이건 수업에서 배웠어!
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
